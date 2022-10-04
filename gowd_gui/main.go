b435f4811e4a157409f2753d907caa49c18e2107
Nipun <shehanatuk@gmail.com>
2022-10-04 14:50:08 -0400

    working on adding CSS styles to modal in GUI

--- gowd_gui/main.go
+++ gowd_gui/main.go
@@ -26,44 +26,18 @@ func main() {
 	second_row := bootstrap.NewRow(bootstrap.NewColumn(bootstrap.ColumnSmall, 3, second_div))
 	body.AddElement(second_row)
 
-	second_row.AddHTML(
-		`
-		
-		<h1>Rules: </h1>
-		<div class="example1 wrapper">
-			<div class="main-item">width: 60%</div>
-			<div class="side-item nav">flex: 1</div>
-			<div class="side-item right-rail">flex: 1</div>
-		</div>
-		<!-- Button trigger modal -->
-		
+	// second_row.AddHTML(
+	// 	`
 
-		<!-- Modal -->
-		<div class="modal" id="modal-one">
-			<div class="modal-bg modal-exit"></div>
-			<div class="modal-container">
-				<h1>Amazing Modal</h1>
-				<h2>Pure Vanilla JavaScript</h2>
-				<button class="modal-close modal-exit">X</button>
-			</div>
-		</div>
-		`, nil)
+	// 	<h1>Rules: </h1>
+	// 	<div class="example1 wrapper">
+	// 		<div class="main-item">width: 60%</div>
+	// 		<div class="side-item nav">flex: 1</div>
+	// 		<div class="side-item right-rail">flex: 1</div>
+	// 	</div>
+	// 	<!-- Button trigger modal -->
 
-	// add a button to show a progress bar
-	btn := bootstrap.NewButton(bootstrap.ButtonPrimary, "Start")
-	create_rule_btn := bootstrap.NewButton(bootstrap.ButtonPrimary, "Open Modal")
-	btn.OnEvent(gowd.OnClick, btnClicked)
-	create_rule_btn.OnEvent(gowd.OnClick, createRuleBtnClicked)
-
-	modal_div = gowd.NewElement("div")
-
-	heading_text := gowd.NewElement("h1")
-	close_modal_btn = gowd.NewElement("button")
-	heading_text.SetText("Modal Text")
-	close_modal_btn.SetText("Close")
-	modal_div.AddElement(heading_text)
-	modal_div.AddElement(close_modal_btn)
-	// modal_div.AddHTML(`
+	// 	<!-- Modal -->
 	// 	<div class="modal" id="modal-one">
 	// 		<div class="modal-bg modal-exit"></div>
 	// 		<div class="modal-container">
@@ -72,14 +46,67 @@ func main() {
 	// 			<button class="modal-close modal-exit">X</button>
 	// 		</div>
 	// 	</div>
-	// `, nil)
+	// 	`, nil)
+	// second_row.AddHTML(
+	// 	`
+	// 	<style>
+	// 		#create_rules_modal {
+	// 			position: fixed;
+	// 			background-color: rgba(255, 255, 255, 0.25);
+	// 			top: 0;
+	// 			right: 0;
+	// 			bottom: 0;
+	// 			left: 0;
+	// 			z-index: 999;
+	// 			visibility: hidden;
+	// 			opacity: 0;
+	// 			pointer-events: none;
+	// 			transition: all 0.3s;
+	// 			&:target {
+	// 			  visibility: visible;
+	// 			  opacity: 1;
+	// 			  pointer-events: auto;
+	// 			}
+	// 			& > div {
+	// 			  width: 400px;
+	// 			  position: absolute;
+	// 			  top: 50%;
+	// 			  left: 50%;
+	// 			  transform: translate(-50%, -50%);
+	// 			  padding: 2em;
+	// 			  background: white;
+	// 			}
+	// 			header {
+	// 			  font-weight: bold;
+	// 			}
+	// 			h1 {
+	// 			  font-size: 150%;
+	// 			  margin: 0 0 15px;
+	// 			}
+	// 		}
+	// 	</style>
+	// 	`, nil)
+
+	// add a button to show a progress bar
+	btn := bootstrap.NewButton(bootstrap.ButtonPrimary, "Start")
+	close_modal_btn = gowd.NewElement("button")
+	create_rule_btn := bootstrap.NewButton(bootstrap.ButtonPrimary, "Open Modal")
+	btn.OnEvent(gowd.OnClick, btnClicked)
+	create_rule_btn.OnEvent(gowd.OnClick, createRuleBtnClicked)
+	close_modal_btn.OnEvent(gowd.OnClick, closeModalBtnClicked)
+
+	modal_div = gowd.NewElement("div")
+
+	heading_text := gowd.NewElement("h1")
+	heading_text.SetText("Modal Text")
+	close_modal_btn.SetText("Close")
+	modal_div.AddElement(heading_text)
+	modal_div.AddElement(close_modal_btn)
+	modal_div.SetID("create_rules_modal")
+
 	modal_div.Hide()
 	row.AddElement(modal_div)
 	row.AddElement(create_rule_btn)
-	// row.AddElement(bootstrap.NewColumn(bootstrap.ColumnSmall, 3, bootstrap.NewElement("div", "well"), modal_div))
-	// row.AddElement(bootstrap.NewColumn(bootstrap.ColumnSmall, 3, bootstrap.NewElement("div", "well", btn)))
-	// row.AddElement(bootstrap.NewColumn(bootstrap.ColumnSmall, 3, bootstrap.NewElement("div", "well", create_rule_btn)))
-	//start the ui loop
 	gowd.Run(body)
 }
 
@@ -116,4 +143,9 @@ func btnClicked(sender *gowd.Element, event *gowd.EventElement) {
 func createRuleBtnClicked(sender *gowd.Element, event *gowd.EventElement) {
 	modal_div.Show()
 	close_modal_btn.Show()
+}
+
+func closeModalBtnClicked(sender *gowd.Element, event *gowd.EventElement) {
+	modal_div.Hide()
+	close_modal_btn.Hide()
 }
