8a466b8c9a13c195f2b0d3890d52b1066ea25946
Nipun <shehanatuk@gmail.com>
2022-10-06 22:12:22 -0400

    fixed error in index.tmpl rendering in frontend package

--- frontend/main.go
+++ frontend/main.go
@@ -1,13 +1,41 @@
 package frontend
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"text/template"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d/%02d/%02d", year, month, day)
+}
+
 func ShowIndexPage() {
 	r := gin.Default()
+	r.Delims("{[{", "}]}")
+	workingDirPath, err := os.Getwd()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	r.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+
+	r.LoadHTMLFiles(workingDirPath + "\\frontend\\html\\index.tmpl")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"now": time.Date(2022, 010, 006, 0, 0, 0, 0, time.UTC),
+		})
+
+	})
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
