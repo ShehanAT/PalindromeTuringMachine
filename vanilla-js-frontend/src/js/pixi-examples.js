5468e70d15ed983c9a64a949aacd89344fff1a42
Nipun <shehanatuk@gmail.com>
2022-10-31 09:04:52 -0400

    added new folder with files: vanilla-js-frontend

---
+++ vanilla-js-frontend/src/js/pixi-examples.js
@@ -0,0 +1,16 @@
+
+const bpc = {};
+
+jQuery(document).ready(($) => {
+
+    bpc.init = function init() {
+        $.getJSON('examples/manifest.json', (data) => {
+            const sections = Object.keys(data);
+            
+        })
+    }
+
+
+
+
+})
